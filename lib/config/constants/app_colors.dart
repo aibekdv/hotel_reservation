20df03482ee924571812565fbe65565c83cb538f
Aibek Karataev <adkarataev@gmail.com>
2023-08-31 16:51:39 +0600

    Initial commit

---
+++ lib/config/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const blue = Color(0xFF0D72FF);
+  static const greyBg = Color(0xFFF6F6F9);
+  static const textGrey = Color(0xFF828796);
+  static const yellow = Color(0x33FFC600);
+  static const yellowDeep = Color(0xFFFFA800);
+}
